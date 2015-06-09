62fe32a5253d2b3685b419c11336e539f3fdf812
Petr Grabovoy <varren@gmail.com>
2015-06-10 02:38:30 +0300

    bugfix of declaredInClass bool

--- src/main/java/ru/varren/RScanner.java
+++ src/main/java/ru/varren/RScanner.java
@@ -1,26 +1,26 @@
 package ru.varren;
 
+import org.reflections.ReflectionUtils;
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
 import org.reflections.scanners.SubTypesScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
 
 import java.lang.reflect.Method;
 import java.util.*;
 
 public class RScanner {
-    private static final String packageToSearch = "ru.varren";
-    // if true will not look for methods declared in super class
-    private static final boolean declaredInClass = false;
+
 
     public static void  main(String [] args) {
         // need to have this lib 'org.reflections:reflections:0.9.10'
         // can download from here gist.github.com/hepin1989/5026900
 
+        // if true will not look for methods declared in super class
+        final boolean declaredInClass = false;
+
         HashMap<String, RScanner.MethodInfo> map =
-                RScanner.findOverloadedMethods(packageToSearch, declaredInClass);
+                RScanner.findOverloadedMethods("ru.varren", declaredInClass);
         RScanner.print(map);
 
         HashMap<String, RScanner.MethodInfo> map2 =
@@ -28,7 +28,6 @@ public class RScanner {
         RScanner.print(map2);
 
 
-        RScanner.print(RScanner.findOverloadedMethods("com.sun", declaredInClass));
     }
 
 
@@ -41,7 +40,9 @@ public class RScanner {
         }
     }
 
-    public static HashMap<String, MethodInfo> findOverloadedMethods(String packageToScan,boolean declaredInClass){
+    public static HashMap<String, MethodInfo>
+            findOverloadedMethods(String packageToScan, boolean declaredInClass){
+
         Set<Class<?>> allClasses = findAllClassesInPackage(packageToScan);
         System.out.println("Number of Classes in "+ packageToScan +" = " + allClasses.size());
 
@@ -49,7 +50,7 @@ public class RScanner {
 
         for (Class c : allClasses) {
             try {
-                findAllMethodsForClass(c, map);
+                findAllMethodsForClass(c, declaredInClass, map);
             }catch (java.lang.NoClassDefFoundError e){
                 //have no idea why some classes throw this NoClassDefFoundError
                 System.err.println(c.getName() + ": java.lang.NoClassDefFoundError");
@@ -58,7 +59,9 @@ public class RScanner {
 
         return map;
     }
-    public static void findAllMethodsForClass(Class c, HashMap<String, MethodInfo> map ) throws java.lang.NoClassDefFoundError{
+    public static void findAllMethodsForClass(Class c, boolean declaredInClass, HashMap<String, MethodInfo> map )
+            throws java.lang.NoClassDefFoundError{
+        //Set<Method> methods = ReflectionUtils.getAllMethods(c);
         Method[] methods = declaredInClass ? c.getDeclaredMethods(): c.getMethods();
 
         for (Method method : methods) {
@@ -67,21 +70,17 @@ public class RScanner {
             if (!map.containsKey(key)) {
                 map.put(key, new MethodInfo(c));
             }
+
             MethodInfo methodInfo = map.get(key);
             methodInfo.methods.add(method);
         }
     }
-    public static Set findAllClassesInPackage(String packageToScan){
-        List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
-        classLoadersList.add(ClasspathHelper.contextClassLoader());
-        classLoadersList.add(ClasspathHelper.staticClassLoader());
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
-                .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
-                .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(packageToScan))));
+    public static Set<Class<?>> findAllClassesInPackage(String packageToScan){
+        Reflections reflections = new Reflections(
+                new SubTypesScanner(false), new ResourcesScanner(),
+                new FilterBuilder().include(FilterBuilder.prefix(packageToScan)));
 
-        Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
-        return allClasses;
+        return reflections.getSubTypesOf(Object.class);
     }
 
     public static void print(HashMap<String, RScanner.MethodInfo> map){
